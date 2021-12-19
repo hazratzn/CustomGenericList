8d445b5dbd90234853372365a77294441a94bd83
unknown <hazratzn@code.edu.az>
2021-12-19 05:55:55 -0800

    Generic List

---
+++ ListProject/ListProject/Models/MyList.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ListProject.Models
+{
+    public class MyList<T> 
+    {
+        
+        public T[] List;
+        public int count;
+        public string list;
+        public MyList()
+        {
+            List = new T[10];
+        }
+        public void ArrayResize()
+        {
+            Array.Resize(ref List, List.Length + 10);
+            
+
+        }
+
+    
+
+        public void Clear(T obj)
+        {
+            var count = 0;
+            for(int i = 0; i<List.Length;i++)
+            {
+                List[count]=List[List.Length-1];
+                count++;
+            }
+            Console.Clear();
+        
+        }
+    
+
+        public void Add(T obj)
+        {
+            if ((count != 0) && (count % 10 == 0))
+            {
+                ArrayResize();
+            }
+            List[count] = obj;
+            count++;
+            Console.WriteLine(obj);
+
+
+
+
+        }
+        
+        public void AddRange(T List)
+        {
+            Add(List);
+        }
+
+        public void FindList(string val)
+        {
+            
+            foreach (var item in List)
+            {
+                Console.WriteLine(item);
+                
+                
+
+              
+            }
+
+        }
+
+
+    }
+        
+        
+    
+}
---
+++ ListProject/ListProject/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using ListProject.Models;
+
+namespace ListProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+            MyList<object> list = new MyList<object>();
+            list.Add("Messi");
+            list.Add("Lionel");
+            list.Add(30);
+            list.Add(10);
+
+            list.AddRange(19);
+
+            //list.Clear(list);
+
+
+            //list.FindList("Messi");
+
+
+
+        }
+    }
+}
